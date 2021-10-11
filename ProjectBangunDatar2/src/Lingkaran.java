d0d4749f6c88162990cbc6bebe74686346769eaf
Lailanailussaadah <71623025+Lailanailussaadah@users.noreply.github.com>
2021-10-11 23:39:01 +0800

    Create Lingkaran.java

---
+++ ProjectBangunDatar2/src/Lingkaran.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author user
+ */
+public class Lingkaran {
+    double hitungLuas(int r){
+        double luas = 22/7 * r * r;
+        return luas;
+    }
+    double hitungLuas(double r){
+        double luas = 22/7 * r * r;
+        return luas;
+    }
+    double hitungKelilings(int r){
+        double keliling = 22/7 * r * 2;
+        return keliling;
+    }
+    double hitungKeliling(double r){
+        double keliling = 22/7 * r * 2;
+        return keliling;
+    }
+}
