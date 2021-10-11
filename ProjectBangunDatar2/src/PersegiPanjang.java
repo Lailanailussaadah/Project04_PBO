1d8838915ff4a0a6714978a525ec1f5465169027
Lailanailussaadah <71623025+Lailanailussaadah@users.noreply.github.com>
2021-10-11 23:39:14 +0800

    Create PersegiPanjang.java

---
+++ ProjectBangunDatar2/src/PersegiPanjang.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author user
+ */
+public class PersegiPanjang {
+    int hitungLuas(int panjang, int lebar){
+        int luas;
+        luas = panjang * lebar;
+        return luas;
+    }
+    double hitungLuas(double panjang, int lebar){
+        double luas;
+        luas = panjang * lebar;
+        return luas;
+    }
+    double hitungLuas(double panjang, double lebar){
+        double luas;
+        luas = panjang * lebar;
+        return luas;
+    }
+    double hitungLuas(int panjang, double lebar){
+        double luas;
+        luas = panjang * lebar;
+        return luas;
+    }
+    int hitungKeliling(int panjang, int lebar){
+        int keliling;
+        keliling = (panjang + lebar)*2 ;
+        return keliling;
+    }
+    double hitungKeliling(double panjang, int lebar){
+        double keliling;
+        keliling = (panjang + lebar)*2;
+        return keliling;
+    }
+    double hitungKeliling(double panjang, double lebar){
+        double keliling;
+        keliling = (panjang + lebar)*2;
+        return keliling;
+    }
+    double hitungKeliling(int panjang, double lebar){
+        double keliling;
+        keliling = (panjang + lebar)*2;
+        return keliling;
+    }
+}
