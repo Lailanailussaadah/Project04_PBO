61671eacf8c735331b1700385f00e3d506d151e9
Lailanailussaadah <71623025+Lailanailussaadah@users.noreply.github.com>
2021-10-11 23:38:50 +0800

    Create Persegi.java

---
+++ ProjectBangunDatar2/src/Persegi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author user
+ */
+public class Persegi {
+    int hitungLuas(int s){
+        int luas =  s * s;
+        return luas;
+    }
+    double hitungLuas(double s){
+        double luas = s * s;
+        return luas;
+    }
+    int hitungKeliling(int s){
+        int keliling = s* 4;
+        return keliling;
+    }
+    double hitungKeliling(double s){
+        double keliling = s * 4;
+        return keliling;
+    }
+}
